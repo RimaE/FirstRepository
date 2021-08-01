10be546c23a382be753c7b4999b5ff8830d10bc2
RimaE <88009519+RimaE@users.noreply.github.com>
2021-08-01 12:28:07 -0400

    Update TEST.java
    u better work

--- src/PracticePackage/TEST.java
+++ src/PracticePackage/TEST.java
@@ -4,6 +4,6 @@ public class TEST {
 	public void testMethod() {
 		//this is git HUb lesson
 		//this line was added from git repository
-		//this is my second line
+		//this is my damn second code 
 	}
 }
